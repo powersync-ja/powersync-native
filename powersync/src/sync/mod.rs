b16bfcf8b92bf78940d7e9c05371aa8f5f5abe88
Simon Binder <simon@journeyapps.com>
2026-09-10 09:12:24 +0200

    Start adopting checkpoint request protocol

--- powersync/src/sync/mod.rs
+++ powersync/src/sync/mod.rs
@@ -1,9 +1,11 @@
+pub mod checkpoint;
 pub mod connector;
 pub mod coordinator;
 pub mod download;
 mod instruction;
 pub mod options;
 pub mod progress;
+mod state;
 pub mod status;
 pub mod stream_priority;
 pub mod streams;
